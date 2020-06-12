3a70a14fc8c22ce4f79ee5477ef66d9e9fee6b2b
Ulmanis <linards.kupcis@gmail.com>
2020-06-12 20:20:19 +0300

    ver 1206 3

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -69,7 +69,7 @@
             this.Controls.Add(this.YellowLight);
             this.Controls.Add(this.RedLight);
             this.Name = "TrafficLights";
-            this.Text = "Form1";
+            this.Text = "Trafficken Lighten";
             ((System.ComponentModel.ISupportInitialize)(this.RedLight)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.YellowLight)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.GreenLight)).EndInit();
